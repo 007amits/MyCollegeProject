fe680a6ae20572317e7f00e1d384c54a7de72212
Amit Shukla <amitshukla5502@gmail.com>
2021-10-06 12:58:35 +0530

    Cleaned the setup

--- src/com/amit/Starter.java
+++ src/com/amit/Starter.java
@@ -1,17 +1,13 @@
 package com.amit;
 
-class Starter
-{
-public static void main(String[] args)
-{
-Reservation r1=new Reservation();
-r1.doMenu();
-
-r1.doBook();
-r1.doCancel();
-r1.doDispList();
-r1.doSearch();
-
-r1.doExit();
-}
+class Starter {
+	public static void main(String[] args) {
+		Reservation r1=new Reservation();
+		r1.doMenu();
+		r1.doBook();
+		r1.doCancel();
+		r1.doDispList();
+		r1.doSearch();
+		r1.doExit();
+	}
 }
